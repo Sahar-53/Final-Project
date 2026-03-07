c1ad6f5ddb52fc3103152daa18b76a5315c75704
Sahar <n.sahar01@outlook.com>
2026-03-07 23:46:30 +0000

    changed tests file location

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,7 +19,7 @@ def client():
 
 # ======== Tests ======== #
 # ===== Test 1 - Regisatration ===== #
-def test_register_user(client):
+def test_registeration_user(client):
     """Test to check if a user is able to register with correct details"""
 
     response = client.post("/register", data={
@@ -29,3 +29,16 @@ def test_register_user(client):
     }, follow_redirects=True)  # redirect to avoid 302 status code
 
     assert response.status_code == 200  # checks that the repsonse status is 200 (success)
+
+
+# ===== Test 2 - Registration ===== #
+def test_regiseration_invalid(client):
+    """Test to check if a user is unable to register with empty username"""
+
+    response = client.post("/register", data={
+        "username": "",
+        "email": "test-user2@gmail.com",
+        "password": "password123"
+    }, follow_redirects=True)  # redirect to avoid 302 status code
+
+    assert b"Username is required" in response.data
